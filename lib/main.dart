bc09499fb76b05f592f42608e543b403c5afd21b
Margret `Pax` Williams <pax.margret@gmail.com>
2020-06-01 05:50:11 -0700

    :sparkles: Receive a stream of data from localhost:8000

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -15,8 +16,35 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final http.Client _client = http.Client();
+
+  MyHomePage() {
+    subscribe();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(child: Text("[DATA HERE]"));
+  }
+
+  subscribe() async {
+    print("Subscribing...");
+    try {
+      var request =
+          new http.Request("GET", Uri.parse("http://localhost:8000/sse"));
+      request.headers["Cache-Control"] = "no-cache";
+      request.headers["Accept"] = "text/event-stream";
+
+      Future<http.StreamedResponse> response = _client.send(request);
+      response.asStream().listen((streamedResponse) {
+        print(
+            "Received streamedResponse.statusCode:${streamedResponse.statusCode}");
+        streamedResponse.stream.listen((data) {
+          print("Received data: $data");
+        });
+      });
+    } catch (e) {
+      print("Caught $e");
+    }
   }
 }
